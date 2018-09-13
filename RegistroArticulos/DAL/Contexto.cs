33f2b249de6c3e7d733599068c3632b53e1dde6b
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-13 15:09:42 -0400

    Creando contexto

---
+++ RegistroArticulos/DAL/Contexto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RegistroArticulos.Entidades;
+
+namespace RegistroArticulos.DAL
+{
+    public class Contexto
+    {
+        public DbSet<Articulo> Articulo { get; set; }
+
+        public Contexto() : base("Constr") { }
+    }
+}
