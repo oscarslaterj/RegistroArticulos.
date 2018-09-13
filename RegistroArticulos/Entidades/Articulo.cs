5aecd1cda19d183ec3ba9121d3b8850758bd6193
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-13 15:06:40 -0400

    Creando Entidad

---
+++ RegistroArticulos/Entidades/Articulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroArticulos.Entidades
+{
+    public class Articulo
+    {
+        [Key]
+
+        public int ArticuloId { get; set; }
+        public DateTime FechaVencimiento { get; set; }
+        public double Precio { get; set; }
+        public int Existencia { get; set; }
+        public int CantidadCotizada { get; set; }
+
+        public Articulo()
+        {
+            ArticuloId = 0;
+            FechaVencimiento = DateTime.Now;
+            Precio = 0;
+            Existencia = 0;
+            CantidadCotizada = 0;
+        }
+        
+    }
+}
