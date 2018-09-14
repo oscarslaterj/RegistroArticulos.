c81cf487f50b4cdd6fa8b2d688e48ef7ea87ad44
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-09-14 14:13:19 -0400

    Creando Test

---
+++ RegistroArticulosTests/BLL/ArticulosBLLTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RegistroArticulos.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistroArticulos.BLL.Tests
+{
+    [TestClass()]
+    public class ArticulosBLLTests
+    {
+        [TestMethod()]
+        public void GetListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ModificarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EliminarTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void BuscarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
